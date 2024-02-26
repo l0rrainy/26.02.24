bc234983bbe4476d83a2d9c12f254edfe289f9bf
l0rrainy <142435583+l0rrainy@users.noreply.github.com>
2024-02-26 12:26:40 -0300

    Add files via upload

---
+++ dowhile.js
@@ -0,0 +1,13 @@
+// Declaração de uma matriz (array) contendo cinco elementos do tipo string.
+var matriz = ["vetor1", "vetor2", "vetor3", "vetor4", "vetor5"];
+
+// inicio da variável 'posicao' com o valor 0 para representar a posição atual na matriz.
+var posicao = 0; 
+
+// Usando um loop 'while' para percorrer a matriz até o final.
+while (posicao < matriz.length) { 
+    // Imprimindo no console o elemento na posição atual da matriz.
+    console.log(matriz[posicao]); 
+    // Incrementando a posição para passar para o próximo elemento na próxima repetição ou seja a cada repetição do loop while, a variável posicao é aumentada em 1.
+    posicao++; 
+}
---
+++ ifelse.js
@@ -0,0 +1,12 @@
+// Declaração de duas variáveis, "x" e "y", e atribuição do valor "2" nas duas.
+var x = 2;
+var y = 2;
+// n alinha abaixo vamos verificar se a variavel x é igual ao número dentro da variavel y.
+if (x == y) {
+    // Se os numeros forem iguais, exibe no console a mensagem "Números conferem!".
+    console.log("Números conferem!");
+} else {
+    // Se os numeros forem diferentes, exibe no console a mensagem "Números divergem!".
+    console.log("Números divergem!");
+}
+
