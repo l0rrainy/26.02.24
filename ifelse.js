ece2a9b4fba36d518dc3cdb714c755ddd5c74994
l0rrainy <142435583+l0rrainy@users.noreply.github.com>
2024-02-26 12:30:39 -0300

    Update ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -1,13 +1,13 @@
-// Declaração de duas variáveis, "x" e "y", e atribuição do valor "2" nas duas.
+// Declaração de duas variáveis, "x" e "y", e atribuição do valor "2" nas duas;
 var x = 2;
 var y = 2;
-// n alinha abaixo vamos verificar se a variavel x é igual ao número dentro da variavel y.
+// n alinha abaixo vamos verificar se a variavel x é igual ao número dentro da variavel y;
 if (x == y) {
-    // Se os numeros forem iguais, exibe no console a mensagem "Números conferem!".
+    // Se os numeros forem iguais, exibe no console a mensagem "Números conferem!";
     console.log("Números conferem!");
     // a linha abaixo encerra o bloco if e começa o else;
 } else {
-    // Se os numeros forem diferentes, exibe no console a mensagem "Números divergem!".
+    // Se os numeros forem diferentes, exibe no console a mensagem "Números divergem!";
     console.log("Números divergem!");
     // a linha abaixo encerra o bloco else;
 }
